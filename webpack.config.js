2aabe09994c5f32a4e465caf8faadd9b8af0352b
sscchan <stephen.s.c.chan@gmail.com>
2017-01-29 20:23:43 -0800

    add webpack configuration to suit current directory structure

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'client/dist');
+var APP_DIR = path.resolve(__dirname, 'client');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        presets: ['es2015', 'react']
+      }
+    ]
+  },
+  devServer: { 
+    inline: true,
+  }
+};
+
+module.exports = config;
