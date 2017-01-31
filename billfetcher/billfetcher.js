f573e714eb2840614c5cd82fb75517dbb3b5cf50
James Falkoff <james.falkoff@gmail.com>
2017-01-31 15:29:09 -0800

    clean up code and add more comments to code

--- billfetcher/billfetcher.js
+++ billfetcher/billfetcher.js
@@ -1,11 +1,6 @@
-var request = require('request');
 var mongoose = require('mongoose');
-var fs = require('fs');
-
 var config = require('./config');
 var helpers = require('./httpHelpers');
-var Bill = require('./../db/models/bill');
-var logger = require('./logHelpers.js');
 
 mongoose.connect(config.databaseURL);
 
@@ -16,41 +11,6 @@ helpers.initializeBillsDatabase(function() {
   });
 });
 
-/// Code below this line is WIP
-
-/*
-// find the newest record in the database so we can only fetch records that are newer
-Bill.findOne({}, {}, {sort: {'created_at': -1 }}, function(err, bill) {
-  if (err) {
-    logger.log('Error finding the most recent bill in billfetcher.js');
-  } else {
-    var introducedOn = bill.introducedOn;  
-    logger.log('Found newest record in database, introduced on ' + introducedOn, function() {});
-
-    // Get all bills introduced on a date equal to or later than the most recent date in the database
-    
-
-    // Check if bill is already in database, and if not add it to database.
-    // Some bills may already be in the database because the precision of 'bill.introducedOn' is only 
-    // to the day. For example, we might have checked for new bills in the morning and put them in the 
-    // database, but there could be additional bills from the same day if we check in the evening.
-
-
-
-    // // Parse the 'introduced on' date.
-    // // The API returns dates in the form yyyy-mm-dd
-    // var year = Number(introducedOn.substring(0, 4));
-    // var month = Number(introducedOn.substring(5, 7));
-    // var day = Number(introducedOn.substring(8, 10));
-    // // Convert the string representation into a Javascript Date
-    // introducedOn = new Date(year, month, day);
-
-  }
-
-
-
-});
-*/
 
 
 
--- billfetcher/httpHelpers.js
+++ billfetcher/httpHelpers.js
@@ -18,23 +18,72 @@ var updateQueryString = {
   per_page: '20'
 };
 
-// Generic helper function for querying the bills API.
-// First argument is a query string object.
-// Second argument is a callback function with the following signature: function(error, response, body). 
-var billsAPIRequest = function(qs, callback) {
-  request({
-    url: config.CONGRESS_API_URL,
-    qs: qs,
-    method: 'GET'
-  }, function(error, response, body) {
-    if (error) {
-      callback(error, null, null);
+
+// This function updates the bills database with any new bills.
+// It is meant to be run on an ongoing basis via cron job, but can be run ad hoc as well.
+var updateBillsDatabase = function(callback) {
+  // find the most recently added bill in the database
+  Bill.findOne({}, {}, {sort: {'created_at': -1 }}, function(err, bill) {
+    if (err) {
+      logger.log('Error finding the most recent bill in the database: ' + err);
     } else {
-      callback(null, response, body);
+      var mostRecentInDatabase = bill.introduced_on; 
+
+      // Now retrieve all bills introduced on or after the most recent bill in our database and iterate through them, 
+      // adding them to the database if they aren't already in it.
+      // Some bills may already be in the database because the API does not provide timing precision greater than a day. 
+      // For example, if the newest bill in our database was introduced on 1/27/2017, there may be other bills from 1/27/2017 
+      // that are not in our database yet, but we cannot do a more precise search to identify only these newer bills, so we
+      // must look at all bills from 1/27/2017 onward.
+
+      // First, do an initial request to figure out how many new bills we need to download into the database
+      updateQueryString.introduced_on__gte = mostRecentInDatabase;      // this instructs the API to only return bills
+                                                                        // introduced on or after a certain date
+      billsAPIRequest(updateQueryString, function(error, response, body) {
+        if (error) {
+          logger.log('Error updating Bills database: ' + err); 
+        } else {
+          retrieveAndStore(JSON.parse(body).count, updateQueryString, callback);
+        }
+      });
     }
-  });  
+  });
 };
 
+
+// This function initializes the bills database if it has not been initialized yet.
+// It checks if the database is empty, and if so it retrieves all bills from the current Congress.
+var initializeBillsDatabase = function(callback) {
+
+   // Check if the database is empty
+  Bill.findOne({}, {}, function(err, bill) {
+    if (err) {
+      logger.log('Error initializing Bills database: ' + err);
+    } else if (!bill) { 
+      // The database is empty, so grab all recent congressional bills...
+
+      // First, do an initial request to figure out how many bills we need to download into the database
+      billsAPIRequest(initializationQueryString, function(error, response, body) {
+        if (error) {
+          logger.log('Error initializing Bills database: ' + err); 
+        } else {
+          retrieveAndStore(JSON.parse(body).count, initializationQueryString, callback); 
+        }
+      });
+    } else { // database is not empty, no initialization needed
+      callback();
+    }
+  });
+};
+
+
+// This helper function will make multiple calls to the Sunlight Foundation API to retrieve a set of bills, and
+// then store those bills in the database. The helper function is designed primarily to abstract away the details 
+// of paginated responses from the API.
+// Arguments:
+// billCount: The number of bills to retrieve
+// qs: A base query string for the API call
+// callback: A callback that is invoked when all bills have been retrieved and written to the database
 var retrieveAndStore = function(billCount, qs, callback) {
 
   var billsFetched = 0;         // running count of bills we have requested from the API so far
@@ -71,62 +120,24 @@ var retrieveAndStore = function(billCount, qs, callback) {
 };
 
 
-// This function updates the bills database with any new bills.
-// It is meant to be run on an ongoing basis via cron job, but can be run ad hoc as well.
-var updateBillsDatabase = function(callback) {
-  // find the most recently added bill in the database
-  Bill.findOne({}, {}, {sort: {'created_at': -1 }}, function(err, bill) {
-    if (err) {
-      logger.log('Error finding the most recent bill in the database: ' + err);
+// Generic helper function for querying the bills API.
+// First argument is a query string object.
+// Second argument is a callback function with the following signature: function(error, response, body). 
+var billsAPIRequest = function(qs, callback) {
+  request({
+    url: config.CONGRESS_API_URL,
+    qs: qs,
+    method: 'GET'
+  }, function(error, response, body) {
+    if (error) {
+      callback(error, null, null);
     } else {
-      var mostRecentInDatabase = bill.introduced_on; 
-      console.log('Found newest bill in database, introduced on ' + mostRecentInDatabase);
-
-      // Now retrieve all bills introduced on or after the most recent bill in our database and iterate through them, 
-      // adding them to the database if they aren't already in it.
-      // Some bills may already be in the database because the API does not provide timing precision greater than a day. 
-      // For example, if the newest bill in our database was introduced on 1/27/2017, there may be other bills from 1/27/2017 
-      // that are not in our database yet, but we cannot do a more precise search to identify only these newer bills, so we
-      // must look at all bills from 1/27/2017 onward.
-
-      // First, do an initial request to figure out how many new bills we need to download into the database
-      updateQueryString.introduced_on__gte = mostRecentInDatabase;      // this instructs the API to only return bills
-                                                                        // introduced on or after a certain date
-      billsAPIRequest(updateQueryString, function(error, response, body) {
-        if (error) {
-          logger.log('Error updating Bills database: ' + err); 
-        } else {
-          retrieveAndStore(JSON.parse(body).count, updateQueryString, callback);
-        }
-      });
+      callback(null, response, body);
     }
-  });
+  });  
 };
 
-// This function initializes the bills database if it has not been initialized yet.
-// It checks if the database is empty, and if so it retrieves all bills from the current Congress.
-var initializeBillsDatabase = function(callback) {
 
-   // Check if the database is empty
-  Bill.findOne({}, {}, function(err, bill) {
-    if (err) {
-      logger.log('Error initializing Bills database: ' + err);
-    } else if (!bill) { 
-      // The database is empty, so grab all recent congressional bills...
-
-      // First, do an initial request to figure out how many bills we need to download into the database
-      billsAPIRequest(initializationQueryString, function(error, response, body) {
-        if (error) {
-          logger.log('Error initializing Bills database: ' + err); 
-        } else {
-          retrieveAndStore(JSON.parse(body).count, initializationQueryString, callback); 
-        }
-      });
-    } else { // database is not empty, no initialization needed
-      callback();
-    }
-  });
-};
 
 module.exports.initializeBillsDatabase = initializeBillsDatabase;
 module.exports.updateBillsDatabase = updateBillsDatabase;
