bd465e857e19ec49a53c930187e0ceaddc4bcc6b
sscchan <stephen.s.c.chan@gmail.com>
2017-01-31 13:05:24 -0800

    implement SearchBar components for legislation search

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,49 @@
+var React = require('react');
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.userSearchTerms = '';
+
+    this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSearchInputChange(event) {
+    this.userSearchTerms = event.target.value;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.userSearchTerms);
+  }
+
+
+  render() {
+    return (
+      <SearchBarPresentational onSearchInputChange={this.handleSearchInputChange} onSubmit={this.handleSubmit} />
+    )
+  }
+}
+
+class SearchBarPresentational extends React.Component {
+  render() {
+    return (
+      <div>
+        <form className="form-group" onSubmit={this.props.onSubmit}>
+          <label htmlFor="searchTerms">Search by Keywords:</label><br />
+          <div className="input-group">
+            <span className="input-group-addon"><span className="glyphicon glyphicon-search" aria-hidden="true"></span></span>
+            <input className="form-control" type="text" id="searchTerms" placeholder="Enter Keywords" onChange={this.props.onSearchInputChange}></input>
+             <span className="input-group-btn">
+                <button type="submit" className="btn btn-default">Search</button>
+             </span>   
+          </div>           
+        </form> 
+      </div>
+    );
+  }
+}
+
+module.exports = SearchBar;
