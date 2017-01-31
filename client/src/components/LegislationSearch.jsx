9fe347c88d7422ca4452ff5f9ddf8bb515620c32
sscchan <stephen.s.c.chan@gmail.com>
2017-01-31 15:57:02 -0800

    implement search keyword function (no words associations yet)

--- client/src/components/LegislationSearch.jsx
+++ client/src/components/LegislationSearch.jsx
@@ -3,10 +3,50 @@ const SearchBar = require('./SearchBar.jsx');
 const SearchResults = require('./SearchResults.jsx');
 
 class LegislationSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetchingSearchResults: false,
+      searchResults: []
+    };
+
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  }
   render() {
     return (
-      <LegislationSearchPresentational />
+      <LegislationSearchPresentational 
+        isFetching={this.state.isFetchingSearchResults}
+        billResults={this.state.searchResults} 
+        onSearchSubmit={this.handleSearchSubmit}
+      />
     );
+  }
+
+  handleSearchSubmit(searchTerms) {
+    this.setState({isFetchingSearchResults: true});
+
+    let ajaxSettings = {
+      method: 'GET',
+      context: this,
+      data: {
+        query: searchTerms,
+        fields: 'bill_id,bill_type,chamber,introduced_on,last_action_at,short_title,official_title,keywords,summary_short,urls,sponsor,sponsor_id,cosponsor_ids,cosponsors.legislator,related_bill_ids,upcoming'
+      },
+      dataType: 'jsonp',
+      success: this.handleSearchComplete.bind(this)
+
+    };
+
+    $.ajax('https://congress.api.sunlightfoundation.com/bills/search', ajaxSettings);
+  }
+
+  handleSearchComplete(data) {
+    // console.log(data.results);
+    this.setState({
+      isFetchingSearchResults: false,
+      searchResults: data.results
+    });
+    console.log(this.state);
   }
 }
 
@@ -14,8 +54,10 @@ class LegislationSearchPresentational extends React.Component {
   render() {
     return (
       <div>
-        <SearchBar />
-        <SearchResults />
+        <SearchBar onSubmit={this.props.onSearchSubmit}/>
+        {!this.props.isFetching &&
+          <SearchResults isFetching={this.props.isFetching} billResults={this.props.billResults}/>
+        }
       </div>
     );
   }
