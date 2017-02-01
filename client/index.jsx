a378c2799d805cf557663bc3e61ca6bfac71676c
sscchan <stephen.s.c.chan@gmail.com>
2017-01-31 17:39:33 -0800

    add legislatorCache mechanism and load pre-downloaded data

--- client/index.jsx
+++ client/index.jsx
@@ -2,8 +2,18 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const UserLegislatorsInfo = require(__dirname + '/src/components/UserLegislatorsInfo.jsx');
 const LegislationSearch = require(__dirname + '/src/components/LegislationSearch.jsx');
+const LegislatorData = require(__dirname + '/src/data/LegislatorData.js');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This is an cache for storing legislator info retrieved from the Sunlight API.
+    // This is design to reduce the number of API call to sunlight for legislator information
+
+    this.legislatorInfoCache = LegislatorData;
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -12,7 +22,7 @@ class App extends React.Component {
             <UserLegislatorsInfo />
           </div>
           <div className="col-md-8">
-            <LegislationSearch />
+            <LegislationSearch legislatorCache={this.legislatorInfoCache}/>
           </div>
         </div>
       </div>
