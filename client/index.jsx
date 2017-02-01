e8acf4c6d13a579b0ee3e33dbc3246f3fc76cfc0
sscchan <stephen.s.c.chan@gmail.com>
2017-01-31 19:55:17 -0800

    add navbar component to front page

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,12 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router');
+const Link = ReactRouter.Link;
+const Router = ReactRouter.Router;
+const IndexRoute = ReactRouter.IndexRoute;
+const hashHistory = ReactRouter.hashHistory;
+
+const NavigationBar = require(__dirname + '/src/components/NavigationBar.jsx');
 const UserLegislatorsInfo = require(__dirname + '/src/components/UserLegislatorsInfo.jsx');
 const LegislationSearch = require(__dirname + '/src/components/LegislationSearch.jsx');
 const LegislatorData = require(__dirname + '/src/data/LegislatorData.js');
@@ -16,13 +23,16 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-4">
-            <UserLegislatorsInfo />
-          </div>
-          <div className="col-md-8">
-            <LegislationSearch legislatorCache={this.legislatorInfoCache}/>
+      <div>
+        <NavigationBar />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <UserLegislatorsInfo />
+            </div>
+            <div className="col-md-8">
+              <LegislationSearch legislatorCache={this.legislatorInfoCache}/>
+            </div>
           </div>
         </div>
       </div>
