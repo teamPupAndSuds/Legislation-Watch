681fc7a62d5b5d347581af10ee38e4acdcfc7a94
sscchan <stephen.s.c.chan@gmail.com>
2017-02-01 07:46:01 -0800

    add placeholder component for UserDashBoard as React routing destination

---
+++ client/src/components/UserDashBoard.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class UserDashBoard extends React.Component {
+  render() {
+    return (
+      <div>User Dash Board Page </div>
+    );
+  }
+}
+
+module.exports = UserDashBoard;
