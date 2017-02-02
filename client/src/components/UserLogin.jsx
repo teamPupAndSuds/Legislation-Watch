cdbd7eb30786df9736b88fece2894e181dbdbe91
sscchan <stephen.s.c.chan@gmail.com>
2017-02-01 17:19:10 -0800

    implmenet AJAX call to server for login

--- client/src/components/UserLogin.jsx
+++ client/src/components/UserLogin.jsx
@@ -6,17 +6,40 @@ class UserLogin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoginError: false
+      isLoginError: false,
+      loginErrorMessage: ''
     };
+
+    this.formFields = {};
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleInputFieldChange = this.handleInputFieldChange.bind(this);
   }
 
-  handleUserNameChange(event) {
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    let loginUserInfo = {};
+    loginUserInfo.username = this.formFields.username;
+    loginUserInfo.password = this.formFields.password;
+
+    // Send a POST request to signin
+    $.post('login/', loginUserInfo)
+      .done(function(data) {
+        hashHistory.push('/dashboard');
+      })
+      .fail(error => {
+        this.setState({
+          isLoginError: true,
+          loginErrorMessage: error.status + '-' + error.statusText
+        });
+      });
 
   }
 
-  handlePasswordChange(event) {
-
-  }
+  handleInputFieldChange(event) {
+    this.formFields[event.target.id] = event.target.value;
+  }  
 
   render() {
     return (
@@ -25,24 +48,25 @@ class UserLogin extends React.Component {
           <h4>Login</h4>
         </div>
         <div className="panel-body">
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input type="text" className="form-control" id="username" placeholder="Enter Username" onChange={this.handleInputFieldChange}></input>
 
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input type="text" className="form-control" id="username" placeholder="Enter Username" onChange={this.handleUserNameChange}></input>
+            </div>
+            <div className="form-group">
 
-          </div>
-          <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" className="form-control" id="password" placeholder="Enter Password" onChange={this.handleInputFieldChange}></input>
 
-            <label htmlFor="password">Password:</label>
-            <input type="password" className="form-control" id="password" placeholder="Enter Password" onChange={this.handlePasswordChange}></input>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">Login</button>
+              {(this.state.isLoginError) ? <h5 style={{'color': 'red'}}>Login Failure: {this.state.loginErrorMessage}</h5> : <p></p> }
+            </div>
 
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-primary">Login</button>
-            {(this.state.isLoginError) ? <h5 style={{'color': 'red'}}>Login Failure</h5> : <p></p> }
-          </div>
-
-          or <Link to="/signup">Signup</Link> 
+            or <Link to="/signup">Signup</Link> 
+          </form>
 
         </div>
       </div>
