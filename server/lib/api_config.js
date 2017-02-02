8345f76ae920b10113c82bbe31df4679984ec49c
Benze Gong <benzebgong@gmail.com>
2017-02-01 22:24:28 -0800

    Update api_config.js

--- server/lib/api_config.js
+++ server/lib/api_config.js
@@ -1,5 +1,5 @@
 exports.wordAssocAPIk = 
-{'key': '8KmkzUb9EimshHx79k94P5FwiuMyp1F5QfpjsnnLO1PO56oj2J'};
+{'key': ''};
 
 exports.geoCode = 
-{'key': 'AIzaSyC-95LSE5kfVWPuFIKruiE0BWOV9phPTUc'};
+{'key': ''};
