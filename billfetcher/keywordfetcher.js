47046545c7467ff1021b9b0352c85888d6928d43
James Falkoff <james.falkoff@gmail.com>
2017-02-02 15:35:58 -0800

    improve keywordfetcher to be more fault tolerant

--- billfetcher/keywordfetcher.js
+++ billfetcher/keywordfetcher.js
@@ -10,56 +10,71 @@ var fetchBillsKeywords = function(callback) {
   // check flat file for the last time this module was run
   fs.readFile(config.keywordFetcherTimestampFile, function(err, data) {
     var timestamp;
-    if (err && err.code !== 'ENOENT') { // check if there is an error but exclude the case where the file doesn't exist
-      logger.log('Error reading from keywordFetcherTimestampFile: ' + err);
-    } else {
-      if (err) { // file doesn't exist, therefore this module has never been run before
-        timestamp = (new Date(0)).toISOString();
-      } else { // this module has been run before, so check the timestamp of the last time it ran     
-        timestamp = data.toString(); // this is the last time the module was run, in ISO string format
+    if (err) {
+      if (err.code !== 'ENOENT') { // if err.code === 'ENOENT', the file doesn't exist, which is OK
+        logger.log('Error reading from keywordFetcherTimestampFile: ' + err);
       }
+      timestamp = (new Date(0)).toISOString();
+    } else { // this module has been run before, so check the timestamp of the last time it ran     
+      timestamp = data.toString(); // this is the last time the module was run, in ISO string format
+    }
 
-      // get any bills from the database that were inserted/updated more recently than the last time this module was run
-      Bill.find({'updatedAt': { $gt: new Date(timestamp) }}, function(err, bills) {
-        if (err) {
-          logger.log('Error retrieving bills from the database: ' + err);
-        } else if (bills.length === 0) { // no work to be done
-          writeCurrentTimeToFile(config.keywordFetcherTimestampFile, function() {
-            callback();
-          });
-        } else {
-          var billsSaved = 0;
-          // for each bill, get the text of the bill
-          bills.forEach(function(bill) {
-            getBillText(bill, function(text) {
-              // use the text of the bill to get keywords via API
-              getBillKeywords(text, function(results) {
-                // save keywords back to the bills in the database
-                if (!results.keyword) {
-                  results.keyword = {};
-                }
-                saveKeywords(bill, Object.keys(results.keyword), function() {
-                  billsSaved++;
-                  console.log(billsSaved + ' bills saved.');
-                  if (billsSaved === bills.length) {
-                    writeCurrentTimeToFile(config.keywordFetcherTimestampFile, function() {
-                      callback();
+    // get any bills from the database that were inserted/updated more recently than the last time this module was run
+    Bill.find({'updatedAt': { $gt: new Date(timestamp) }}, function(err, bills) {
+      if (err) {
+        logger.log('Error retrieving bills from the database: ' + err);
+        callback(err);
+      } else if (bills.length === 0) { // no work to be done
+        writeCurrentTimeToFile(config.keywordFetcherTimestampFile, function() {
+          callback();
+        });
+      } else {
+        var billsSaved = 0;
+        // for each bill, get the text of the bill
+        bills.forEach(function(bill) {
+          if (bill.keywords_generated.length) { // we've already generated keywords for this bill at some time in the past
+            checkIfDone(++billsSaved, bills.length, callback);           
+          } else {
+            getBillText(bill, function(err, text) {
+              if (err) {
+                checkIfDone(++billsSaved, bills.length, callback);
+              } else {
+                // use the text of the bill to get keywords via API
+                getBillKeywords(text, function(err, results) {
+                  // save keywords back to the bills in the database
+                  if (err) {
+                    checkIfDone(++billsSaved, bills.length, callback);                   
+                  } else {
+                    if (!results.keyword) {
+                      results.keyword = {};
+                    }
+                    saveKeywords(bill, Object.keys(results.keyword), function(err) {
+                      checkIfDone(++billsSaved, bills.length, callback); 
                     });
                   }
-                }); 
-              });
+                });
+              }
             });
-          });
-        }
-      });
-    }
+          }
+        });
+      }
+    });
   });
+};
+
+var checkIfDone = function(billsSaved, totalBills, callback) {
+  console.log('Processed bill number: ' + billsSaved);
+  if (billsSaved === totalBills) {
+    writeCurrentTimeToFile(config.keywordFetcherTimestampFile, function() {
+      callback();
+    });  
+  }
 };
 
 var getBillText = function(bill, callback) {
   if (!bill.last_version.urls.html) {
     // the text of the bill is not available, so all we have to work with is the title
-    callback(bill.official_title);
+    callback(null, bill.official_title);
   } else {
     requestBillText(bill, callback);  
   }
@@ -71,9 +86,10 @@ var requestBillText = function(bill, callback) {
     method: 'GET'
   }, function(err, response, body) {
     if (err) {
-      logger.log('Error retrieving bill text from URL: ' + err);      
+      logger.log('Error retrieving bill text from URL: ' + err); 
+      callback(err, null);     
     } else {
-      callback(body);
+      callback(null, body);
     }
   });  
 };
@@ -89,7 +105,8 @@ var getBillKeywords = function(text, callback) {
     qs: {text: text}
   }, function(err, response, body) {
     if (err) {
-      logger.log('Error retrieving bill keywords from API: ' + err);      
+      logger.log('Error retrieving bill keywords from API: ' + err); 
+      callback(err, null);     
     } else {
       var results;
       try {
@@ -97,7 +114,7 @@ var getBillKeywords = function(text, callback) {
       } catch (err) {
         results = {};
       }
-      callback(results);
+      callback(null, results);
     }
   });   
 };
@@ -107,8 +124,10 @@ var saveKeywords = function(bill, keywords, callback) {
   bill.save(function(err) {
     if (err) {
       logger.log('Error saving keywords back to database: ' + err);
+      callback(err);
+    } else {
+      callback(null);
     }
-    callback();
   });
 };
 
@@ -123,7 +142,3 @@ var writeCurrentTimeToFile = function(filePath, callback) {
 };
 
 module.exports.fetchBillsKeywords = fetchBillsKeywords;
-
-
-
-
