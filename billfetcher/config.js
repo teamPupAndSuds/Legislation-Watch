22d9728aeca404997e186d9563aa134b12051e1e
James Falkoff <james.falkoff@gmail.com>
2017-02-01 19:44:12 -0800

    add config entry for topic tagging API url

--- billfetcher/config.js
+++ billfetcher/config.js
@@ -1,4 +1,5 @@
 module.exports.CONGRESS_API_URL = 'https://congress.api.sunlightfoundation.com/bills';
+module.exports.TOPIC_TAGGING_API_URL = 'https://twinword-topic-tagging.p.mashape.com/generate/';
 module.exports.databaseURL = 'mongodb://localhost/billfetchertest';
 module.exports.logFile = 'billfetcher/logfile.txt';
 module.exports.keywordFetcherTimestampFile = 'billfetcher/keywordsTimestampFile.txt';
