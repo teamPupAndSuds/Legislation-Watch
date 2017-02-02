11d38831d80a1b5fdb8b0b9031a5754aca16133c
James Falkoff <james.falkoff@gmail.com>
2017-02-02 08:53:05 -0800

    incorporate keyword fetching via twinword API into billfetcher

--- billfetcher/billfetcher.js
+++ billfetcher/billfetcher.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
 var config = require('./config');
 var helpers = require('./httpHelpers');
+var keywordFetcher = require('./keywordfetcher.js');
 
 mongoose.connect(config.databaseURL);
 
 helpers.initializeBillsDatabase(function() {
-  console.log('Initalization complete.');
+  console.log('Database initalization complete.');
   helpers.updateBillsDatabase(function() {
-    console.log('Update complete.');
-    process.exit();
+    console.log('Database update complete.');
+    keywordFetcher.fetchBillsKeywords(function() {
+      console.log('Keyword generation complete.');
+      process.exit();
+    });
   });
 });
 
--- billfetcher/keywordfetcher.js
+++ billfetcher/keywordfetcher.js
@@ -4,23 +4,29 @@ var config = require('./config');
 var apiConfig = require('./../server/lib/api_config');
 var Bill = require('./../db/models/bill');
 var logger = require('./logHelpers.js');
-var mongoose = require('mongoose');
 
 var fetchBillsKeywords = function(callback) {
 
-  mongoose.connect(config.databaseURL);
-
   // check flat file for the last time this module was run
   fs.readFile(config.keywordFetcherTimestampFile, function(err, data) {
-    if (err) {
+    var timestamp;
+    if (err && err.code !== 'ENOENT') { // check if there is an error but exclude the case where the file doesn't exist
       logger.log('Error reading from keywordFetcherTimestampFile: ' + err);
-    } else {      
-      var timestamp = data.toString(); // this is the last time the module was run, in ISO string format
+    } else {
+      if (err) { // file doesn't exist, therefore this module has never been run before
+        timestamp = (new Date(0)).toISOString();
+      } else { // this module has been run before, so check the timestamp of the last time it ran     
+        timestamp = data.toString(); // this is the last time the module was run, in ISO string format
+      }
 
       // get any bills from the database that were inserted/updated more recently than the last time this module was run
       Bill.find({'updatedAt': { $gt: new Date(timestamp) }}, function(err, bills) {
         if (err) {
           logger.log('Error retrieving bills from the database: ' + err);
+        } else if (bills.length === 0) { // no work to be done
+          writeCurrentTimeToFile(config.keywordFetcherTimestampFile, function() {
+            callback();
+          });
         } else {
           var billsSaved = 0;
           // for each bill, get the text of the bill
@@ -29,11 +35,16 @@ var fetchBillsKeywords = function(callback) {
               // use the text of the bill to get keywords via API
               getBillKeywords(text, function(results) {
                 // save keywords back to the bills in the database
-                console.log('results.keyword: ' + results.keyword);
+                if (!results.keyword) {
+                  results.keyword = {};
+                }
                 saveKeywords(bill, Object.keys(results.keyword), function() {
                   billsSaved++;
+                  console.log(billsSaved + ' bills saved.');
                   if (billsSaved === bills.length) {
-                    callback();
+                    writeCurrentTimeToFile(config.keywordFetcherTimestampFile, function() {
+                      callback();
+                    });
                   }
                 }); 
               });
@@ -43,39 +54,6 @@ var fetchBillsKeywords = function(callback) {
       });
     }
   });
-
-  // call the Twinword API using the title and text 
-
-  // populate keywords_generated in database using results of Twinword API 
-
-};
-
-var saveKeywords = function(bill, keywords, callback) {
-  bill.keywords_generated = keywords;
-  bill.save(function(err) {
-    if (err) {
-      logger.log('Error saving keywords back to database: ' + err);
-    }
-    callback();
-  });
-};
-
-var getBillKeywords = function(text, callback) {
-  request({
-    url: config.TOPIC_TAGGING_API_URL,
-    method: 'GET',
-    headers: {
-      'X-Mashape-Key': apiConfig.topicTaggingAPIk.key,
-      Accept: 'application/json',
-    },
-    qs: { text: text}
-  }, function(err, response, body) {
-    if (err) {
-      logger.log('Error retrieving bill keywords from API: ' + err);      
-    } else {
-      callback(JSON.parse(body));
-    }
-  });   
 };
 
 var getBillText = function(bill, callback) {
@@ -100,7 +78,51 @@ var requestBillText = function(bill, callback) {
   });  
 };
 
-module.exports = fetchBillsKeywords;
+var getBillKeywords = function(text, callback) {
+  request({
+    url: config.TOPIC_TAGGING_API_URL,
+    method: 'GET',
+    headers: {
+      'X-Mashape-Key': apiConfig.topicTaggingAPIk.key,
+      Accept: 'application/json',
+    },
+    qs: {text: text}
+  }, function(err, response, body) {
+    if (err) {
+      logger.log('Error retrieving bill keywords from API: ' + err);      
+    } else {
+      var results;
+      try {
+        results = JSON.parse(body);
+      } catch (err) {
+        results = {};
+      }
+      callback(results);
+    }
+  });   
+};
+
+var saveKeywords = function(bill, keywords, callback) {
+  bill.keywords_generated = keywords;
+  bill.save(function(err) {
+    if (err) {
+      logger.log('Error saving keywords back to database: ' + err);
+    }
+    callback();
+  });
+};
+
+var writeCurrentTimeToFile = function(filePath, callback) {
+  var newTimestamp = (new Date()).toISOString();
+  fs.writeFile(config.keywordFetcherTimestampFile, newTimestamp, function(err) { 
+    if (err) {
+      logger.log('Error writing new timestamp to file: ' + err);
+    }
+    callback();
+  });
+};
+
+module.exports.fetchBillsKeywords = fetchBillsKeywords;
 
 
 
