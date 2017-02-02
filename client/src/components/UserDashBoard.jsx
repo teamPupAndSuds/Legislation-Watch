29e9d48d767d56f4ca9669bbebc5ea6b2b071ec6
sscchan <stephen.s.c.chan@gmail.com>
2017-02-01 21:50:09 -0800

    added rendering of mointored word results

--- client/src/components/UserDashBoard.jsx
+++ client/src/components/UserDashBoard.jsx
@@ -1,16 +1,40 @@
 const React = require('react');
 const UserDashBoardKeywordsEntryBar = require('./UserDashBoardKeywordsEntryBar.jsx');
+const UserDashBoardMonitoredWordResult = require('./UserDashBoardMonitoredWordResult.jsx');
 
 class UserDashBoard extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
     return (
       <div>
         <h3>Dash Board</h3>
         <UserDashBoardKeywordsEntryBar />
+
+        {this.props.monitoringResults.map(function(bill) {
+          return <UserDashBoardMonitoredWordResult key={bill.word} monitoredWords={bill.word} relevantBills={bill.relevantBills} />;
+        })}
+
       </div>
 
     );
   }
 }
+
+UserDashBoard.defaultProps = {
+  monitoringResults: [
+    {
+      word: 'turtle',
+      relevantBills: ['hr1761-112', 'hr2276-114', 'hr2279-114']
+    },
+    {
+      word: 'camel',
+      relevantBills: ['s2222-111', 'hr4526-112', 's2311-112', 's2312-112']
+    }
+  ]
+};
 
 module.exports = UserDashBoard;
