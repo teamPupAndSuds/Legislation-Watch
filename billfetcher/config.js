4fd0d7c191979a597540f554d1a5daf9a7d80522
James Falkoff <james.falkoff@gmail.com>
2017-02-01 19:09:35 -0800

    add config entry for keywords timestamp file

--- billfetcher/config.js
+++ billfetcher/config.js
@@ -1,4 +1,5 @@
 module.exports.CONGRESS_API_URL = 'https://congress.api.sunlightfoundation.com/bills';
 module.exports.databaseURL = 'mongodb://localhost/billfetchertest';
 module.exports.logFile = 'billfetcher/logfile.txt';
+module.exports.keywordFetcherTimestampFile = 'billfetcher/keywordsTimestampFile.txt';
 module.exports.congress = 115;
