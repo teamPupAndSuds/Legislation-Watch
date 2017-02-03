e3ab637b57fca8c1ec7010631be9efaa076cf26a
sscchan <stephen.s.c.chan@gmail.com>
2017-02-02 17:54:44 -0800

    implement dashboard result summary with hooks Sunlight foundation API for populating bill info

--- client/src/components/UserDashBoardMonitoredWordResult.jsx
+++ client/src/components/UserDashBoardMonitoredWordResult.jsx
@@ -1,11 +1,56 @@
 const React = require('react');
+const BillResultSummary = require('./BillResultSummary.jsx');
 
 class UserDashBoardMonitoredWordResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetching: true,
+      billData: []
+    };
+
+    this.handleMonitoredWordsRemove = this.handleMonitoredWordsRemove.bind(this);
+  }
+
+  componentDidMount() { 
+    // retrieve the bill information from Sunlight API for all of the supplied billIds
+    let requestQueryString = {
+      'bill_id__in': this.props.billIds.join('|'),
+      fields: 'bill_id,bill_type,chamber,introduced_on,last_action_at,short_title,official_title,keywords,summary_short,urls,sponsor,sponsor_id,cosponsor_ids,cosponsors.legislator,related_bill_ids,upcoming',
+      'per_page': 50
+    };
+
+    $.get('https://congress.api.sunlightfoundation.com/bills', requestQueryString, onGetRelevantBillsComplete.bind(this), 'jsonp');
+
+    function onGetRelevantBillsComplete(data) {
+      this.setState({
+        isFetching: false,
+        billData: data.results
+      });
+    }
+  }
+
+  handleMonitoredWordsRemove(event) {
+    this.props.onMonitoredWordsRemove(this.props.monitoredWords);
+  }
+
   render() {
     return (
       <div>
-        <h4>{this.props.monitoredWords}</h4>
-        {this.props.relevantBills}
+        <div>
+          <div className="pull-left">
+            <h3>
+              + {this.props.monitoredWords}
+            </h3>
+          </div>
+          <div className="pull-right">
+            <h3 className="text-right"><span className=" glyphicon glyphicon-remove-sign" onClick={this.handleMonitoredWordsRemove}></span></h3>
+          </div>
+          <div className="clearfix"></div>
+        </div>
+
+        {this.state.isFetching ? <p> Loading </p> : <p> </p>}
+        {!this.state.isFetching ? this.state.billData.map(bill => <BillResultSummary key={bill.bill_id} info={bill} />) : <p> </p>}
       </div>
     );
   }
