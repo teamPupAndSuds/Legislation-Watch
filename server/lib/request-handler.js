f36da7a899aca1cb720397f8d2deb408602080f4
sscchan <stephen.s.c.chan@gmail.com>
2017-02-03 18:07:40 -0800

    fix endpoint for keyword request

--- server/lib/request-handler.js
+++ server/lib/request-handler.js
@@ -9,26 +9,36 @@ var util = require('./utility.js');
 //AUTHENTICATION
 
 exports.userLogin = function(req, res) {
+
+  console.log('request-handler.js: userLogin: entered');  
   var username = req.body.username;
   var password = req.body.password;
+
+  console.log('request-handler.js: userLogin: username is:', username);
+  console.log('request-handler.js: userLogin: password is:', password);
 
   User.findOne( {username: username} )
     .exec(function(err, user) {
       if (!err) {
         if (!user) {
+          console.log('request-handler.js: userLogin: username not found');
           res.status(401);
           res.end();
         } else {
+          console.log('request-handler.js: userLogin: username found, comparing passwords');          
           util.comparePassword(password, user.password, function(err, match) {
             if (match) {
+              console.log('request-handler.js: userLogin: username found, password match');                 
               util.createSession(req, res, user);
             } else {
+              console.log('request-handler.js: userLogin: username found, password DO NOT match');                    
               res.status(401);
               res.end();
             }
           });
         }
       } else {
+        console.log('request-handler.js: userLogin: database error on locating username');    
         res.status(401).send(err);
       }
     });
@@ -45,21 +55,31 @@ exports.userLogout = function(req, res) {
 exports.userSignup = function(req, res) {
   var username = req.params.username;
 
+  console.log('request-handler.js: userSignup: entered');
+
 /////////////////////////////////////////////////////////////////
 //CHECKS USER DB 
+/////////////////////////////////////////////////////////////////
+
   User.findOne({ username: username})
     .exec(function(err, user) {
       if (!user) {
+
+        console.log('request-handler.js: userSignup: username free');
+        console.log('request-handler.js: userSignup: signup data:', req.body);
+
       //If user profile is not taken, creates a new user profile
         var password = req.body['password'];
         var userEmail = req.body['email'];
         var userAddress = req.body['address'];
-        var streetAddress = userAddress['houseNum'];
-        streetAddress = streetAddress.toString();
+
+
         ///////////////////////////////////////////////
         //building location object to save to userdb
+        ///////////////////////////////////////////////
+
         var location = {};
-        location['houseNum'] = streetAddress;
+        location['houseNum'] = userAddress['houseNum'].toString();
         location['street'] = userAddress['streetName'];
         location['city'] = userAddress['city'];
         location['state'] = userAddress['state'];
@@ -69,9 +89,15 @@ exports.userSignup = function(req, res) {
           location: location,
           email: userEmail,
           keywords: {}
-        }
+        };
+
+        console.log('request-handler.js: userSignup: userinfo:', userInfo);
+
         ///////////////////////////////////////////////
-        //building address string to feed to geoCode
+        // building address string to feed to geoCode
+        ///////////////////////////////////////////////
+
+        var streetAddress = userAddress['houseNum'].toString();        
         streetAddress = streetAddress.concat(
           ' ',
           userAddress['streetName'],
@@ -81,6 +107,8 @@ exports.userSignup = function(req, res) {
           userAddress['state']
           );
 
+        console.log('request-handler.js: userSignup: streetAddress:', streetAddress);        
+
         ////////////////////////////////////////////
         util.geoCodeit(res, userInfo, streetAddress, function(err, response) {
           if (!err) {
@@ -88,24 +116,32 @@ exports.userSignup = function(req, res) {
             userInfo['latitude'] = geoLocation.lat;
             userInfo['longitude'] = geoLocation.lng;
             ////////////////////////////////////////////
-            //creates new user and save to DB
+            // creates new user and save to DB
+            ////////////////////////////////////////////
             var newUser = new User(userInfo);
             newUser.save(function(err, newUser) {
               if (err) {
+                console.log('request-handler.js: userSignup: fail to SaveUser to DB');
                 res.status(500).send(err);
               } else {
-              ////////////////////////////////////////////
-              //creates new client session for a successful sign-up
+              /////////////////////////////////////////////////////////
+              // creates new client session for a successful sign-up
+              /////////////////////////////////////////////////////////
+                console.log('request-handler.js: userSignup: save user OK: newUser is:', newUser);
                 util.createSession(req, res, newUser);
               }
             });
           } else {
+            console.log('request-handler.js: userSignup: geolocation failed');
             res.status(401).send('Unable to get user geolocation');
           }
         });
       } else {
         //USER PROFILE IN USE
         res.status(401);
+
+        console.log('request-handler.js: userSignup: user profile already in use');
+
         res.send('Username already in use');
       }
     });
@@ -116,24 +152,43 @@ exports.userSignup = function(req, res) {
 
 exports.insertWordMonitor = function(req, res) {
   var username = req.params.username;
-  var keywords = req.params.keywords;
+  var keywords = req.body.keywords;
   keywords = keywords.trim();
+
+  console.log('request-handler.js: insertWordMonitor: insert keywords:', keywords);
 
   User.findOne( {username: username} ) 
     .exec(function(err, user) {
       if (!err) {
         if (!user) {
+          console.log('request-handler.js: insertWordMonitor: user does not exist', username);
           res.status(401);
           res.end();
         } else {
+          console.log('request-handler.js: insertWordMonitor: user exist', user);
+
+          // Initialize User Keyword if it doesn't already exist
+          if (user['keywords'] === undefined) {
+            user['keywords'] = [];
+          }
+
+          // See if this keyword already exist
           if (user['keywords'][keywords] !== undefined) {
+            // If so, don't need to do anything
+            console.log('request-handler.js: insertWordMonitor: keyword already exist');            
             res.status(200).end();
           } else {
+            // Else we create a new keyword
+            console.log('request-handler.js: insertWordMonitor: adding new keyword', keywords);
             util.keywordBuilder(user, keywords, function(err, user) {
               if (!err) {
                 //call billassociator
-                res.status(200).end();
+
+                // Send the client the user object with the new results
+                console.log('request-handler.js: insertWordMonitor: keyword added, sending user object back to client:', user);      
+                res.status(200).send(user);
               } else {
+                console.log('request-handler.js: insertWordMonitor: keyword adding failed');                   
                 res.status(500).send(err);
               }
             });
@@ -150,7 +205,7 @@ exports.insertWordMonitor = function(req, res) {
 
 exports.deleteWordMonitor = function(req, res) {
   var username = req.params.username;
-  var keywords = req.params.keywords;
+  var keywords = req.body.keywords;
   keywords = keywords.trim();
 
   User.findOne( {username: username} )
