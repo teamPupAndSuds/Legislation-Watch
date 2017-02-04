3e07f750eda9f9107a0d68c3b02a1b2f39471a97
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-03 17:56:57 -0800

    add function to retrieve current date

--- server/lib/emailWorker.js
+++ server/lib/emailWorker.js
@@ -10,15 +10,32 @@ let transporter = nodemailer.createTransport({
   }
 });
 
+var formatDate = function() {
+  var d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  } 
+  return [year, month, day].join('-');
+};
+
 exports.sendMail = function(userObj, cb) {
-  // setup email data with unicode symbols
-  let insertHtml = '';
+  //start construction body of email
+  let insertHtml = "<h1>Here's what's happening today in congress. Visit Legislature Watch for more results!</h1><br><br><h2>This is the first heading</h2><br><br><h2>This is the second heading</h2>";
+
+  var date = formatDate();
 
   let mailOptions = {
     from: '"Legislature Watch" <legislaturewatch@gmail.com>', // sender address
     to: userObj.email, // list of receivers
-    subject: '(Legislature Watch) Your Daily Digest', // Subject line
-    html: '<b>Hello world ?</b>' // html body
+    subject: '(Legislature Watch) Your Daily Digest ' + date, // Subject line
+    html: insertHtml // html body
   };
 
   // send mail with defined transport object
@@ -50,7 +67,24 @@ var user = {
   },
   latitude: 37.8019553,
   longitude: -122.2999646,
-  keywords: {},
+  keywords: { 
+    'abortion': {
+      word: 'abortion',
+      relatedBills: 
+      ['hr217-115',
+       'hr524-115',
+       'hr277-115',
+       'hr354-115',
+       'hr352-115',
+       'hr147-115',
+       'hr37-115',
+       'hr7-115',
+       'hr586-115',
+       'hr36-115',
+       'hr771-115'] 
+    },
+
+  },
   email: 'c.bathgate1@gmail.com'
 };
 
