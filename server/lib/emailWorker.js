99ed07506b91cd43c205b8d74d9b26b1948a9ef3
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-03 20:14:20 -0800

    an email with a bill title sends

--- server/lib/emailWorker.js
+++ server/lib/emailWorker.js
@@ -1,5 +1,10 @@
 var nodemailer = require('nodemailer');
+var Bill = require('./../../db/models/bill');
+var mongoose = require('mongoose');
 var emailPassword = require('./api_config.js');
+
+//comment me out when we know where this db is going to be initialized
+mongoose.connect('mongodb://localhost/billfetchertest');
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
@@ -25,25 +30,72 @@ var formatDate = function() {
   return [year, month, day].join('-');
 };
 
+//mvp way of adding the bills to email
+var addBills = function(keywordObj, date, cb) {
+  var result = '';
+  var keys = Object.keys(keywordObj);
+  var tasksToGo = keys.length;
+  if (tasksToGo === 0) {
+    cb(null, '<h2>Please add topics to your profile. Visit Legislature Watch for more info</h2>');
+  } else {
+    for (var key in keywordObj) {
+      result += '<h2>' + key + '</h2><br>';
+      for (var i = 0; i < keywordObj[key]['relatedBills'].length; i++) {
+        Bill.findOne({'bill_id': keywordObj[key]['relatedBills'][i]}, function(err, bill) {
+          if (err) {
+            console.log('This is an error in addBills');
+            cb(err);
+          } else {
+            var resultDate = bill['last_version_on'];
+            console.log(key);
+            console.log(i);
+            if (resultDate === date) {
+              result += '<h3>' + bill['official_title'] + '</h3>';
+            }
+            if (--tasksToGo === 0) {
+              cb(null, result);
+            }
+          }
+        });
+      }
+    }
+  }
+};
+
 exports.sendMail = function(userObj, cb) {
   //start construction body of email
-  let insertHtml = "<h1>Here's what's happening today in congress. Visit Legislature Watch for more results!</h1><br><br><h2>This is the first heading</h2><br><br><h2>This is the second heading</h2>";
+  let insertHtml = "<h1>Here's what's happening today in congress. Visit <b>Legislature Watch</b> for more results!<br><br><br>";
 
-  var date = formatDate();
+  //test date 2017-01-09
+  var date = '2017-01-09';
 
-  let mailOptions = {
-    from: '"Legislature Watch" <legislaturewatch@gmail.com>', // sender address
-    to: userObj.email, // list of receivers
-    subject: '(Legislature Watch) Your Daily Digest ' + date, // Subject line
-    html: insertHtml // html body
-  };
+  //use this for the real thing******
+  // var date = formatDate();
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      return console.log(error);
+  addBills(userObj.keywords, date, function(err, result) {
+    if (err) {
+      console.log('Something went wrong');
+      return;
+    } else {
+
+      insertHtml += result;
+
+      let mailOptions = {
+        from: '"Legislature Watch" <legislaturewatch@gmail.com>', // sender address
+        to: userObj.email, // list of receivers
+        subject: '(Legislature Watch) Your Daily Digest. YAY! ' + date, // Subject line
+        html: insertHtml // html body
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+        process.exit();
+      });
     }
-    console.log('Message %s sent: %s', info.messageId, info.response);
   });
 };
 
@@ -68,6 +120,10 @@ var user = {
   latitude: 37.8019553,
   longitude: -122.2999646,
   keywords: { 
+    'bird': {
+      word: 'bird',
+      relatedBills: ['hr368-115']
+    },
     'abortion': {
       word: 'abortion',
       relatedBills: 
@@ -82,8 +138,7 @@ var user = {
        'hr586-115',
        'hr36-115',
        'hr771-115'] 
-    },
-
+    }
   },
   email: 'c.bathgate1@gmail.com'
 };
