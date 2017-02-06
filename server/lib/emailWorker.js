e76025470a36f4d41390be233b7197bc15e6c8e3
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-02-06 12:56:58 -0800

    will only email bill info that was last updated at after the emaillog date

--- server/lib/emailWorker.js
+++ server/lib/emailWorker.js
@@ -2,7 +2,12 @@ var nodemailer = require('nodemailer');
 var Bill = require('./../../db/models/bill');
 var User = require('./../../db/models/user');
 var mongoose = require('mongoose');
+var fs = require('fs');
 var emailPassword = require('./api_config.js');
+
+var emailTimestampFile = 'server/lib/emailTimestamp.txt';
+
+console.log(emailTimestampFile);
 
 mongoose.connect('mongodb://localhost/billfetchertest');
 
@@ -15,8 +20,32 @@ let transporter = nodemailer.createTransport({
   }
 });
 
+//creates the timestamp file
+var createEmailTimestamp = function(date, cb) {
+  fs.writeFile(emailTimestampFile, date, function(err) {
+    if (err) {
+      console.log('Could not write to email timestamp file', err);
+    } else {
+      console.log('in createEmailTimestamp');
+      cb();
+    }
+  });
+};
+
+var readEmailTimestamp = function(cb) {
+  fs.readFile(emailTimestampFile, 'utf8', function(err, data) {
+    if (err) {
+      console.log('There was a problem reading the timestamp file', err);
+      cb(err);
+    } else {
+      cb(null, data);
+    }
+  });
+};
+
 //create date format to match date format in bill table
-var formatDate = function() {
+var formatDate = function(type) {
+
   var d = new Date(),
     month = '' + (d.getMonth() + 1),
     day = '' + d.getDate(),
@@ -31,61 +60,117 @@ var formatDate = function() {
   return [year, month, day].join('-');
 };
 
-var addBills = function(keywordObj, date, cb) {
+var addBills = function(keywordObj, cb) {
   var result = '';
+  var lastBillsSent;
 
+  //get the last 'last_version_on' date that the last call of addBills used from emailLog.txt
+  readEmailTimestamp(function(err, resultTime) {
+    if (err) {
+      console.log('There was an error in readEmailTimestamp function', err);
+    } else {
+      console.log('THIS IS THE RESULT OK!', resultTime);
+      lastBillsSent = resultTime;
+      //format lastBillSent for comparison
+      lastBillsSent = new Date(lastBillsSent);
+      lastBillsSent = lastBillsSent.setDate(lastBillsSent.getDate() + 1);
 
-  //allKeys object acts as a way to keep track of all the keywords 
-  //that have been called on the Bills database so far
-  var allKeys = {};
+      //allKeys object acts as a way to keep track of all the keywords 
+      //that have been called on the Bills database so far
+      var allKeys = {};
 
-  //counts the total amount of keywords the user has
-  //when tasksTotal === Object.keys(allKeys).length, all keys have been looked up and added to result string
-  var keys = Object.keys(keywordObj);
-  var tasksTotal = keys.length;
+      //counts the total amount of keywords the user has
+      //when tasksTotal === Object.keys(allKeys).length, all keys have been looked up and added to result string
+      var keys = Object.keys(keywordObj);
+      var tasksTotal = keys.length;
 
-  //if a user hasn't added topics yet
-  if (tasksTotal === 0) {
-    cb(null, '<h2>Please add topics to your profile. Visit Legislation Watch for more info</h2>');
+      //if a user hasn't added topics yet
+      if (tasksTotal === 0) {
+        cb(null, '<h2>Please add topics to your profile. Visit Legislation Watch for more info</h2>');
 
-  } else {
-    for (var key in keywordObj) {
-      //using an immediately invoked function to keep reference to key name
-      (function(key) {
+      } else {
+        for (var key in keywordObj) {
+          //using an immediately invoked function to keep reference to key name
+          (function(key) {
 
-        //when billsToGo reaches 0, that means all bills in array have been processed
-        var billsToGo = keywordObj[key]['relatedBills'].length;
+            //when billsToGo reaches 0, that means all bills in array have been processed
+            var billsToGo = keywordObj[key]['relatedBills'].length;
 
-        //go through each bill in relatedBills array that has to do with keyword
-        for (var i = 0; i < keywordObj[key]['relatedBills'].length; i++) {
-          Bill.findOne({'bill_id': keywordObj[key]['relatedBills'][i]}, function(err, bill) {
-            if (err) {
-              console.log('This is an error in addBills');
-              cb(err);
-            } else {
-              var resultDate = bill['last_version_on'];
-              if (resultDate === date) {
-                result += "<h2> Keyword: " + key + "</h2>";
-                result += "<h4 style='color:grey;'> Bill ID: " + bill['bill_id'] + "</h2>";
-                result += '<h3>' + bill['official_title'] + '</h3>';
-                result += '<h3>--------------------------</h3>';
-              }
-              billsToGo--;
-              allKeys[key] = key;
+            //go through each bill in relatedBills array that has to do with keyword
+            for (var i = 0; i < keywordObj[key]['relatedBills'].length; i++) {
+              Bill.findOne({'bill_id': keywordObj[key]['relatedBills'][i]}, function(err, bill) {
+                if (err) {
+                  console.log('This is an error in addBills');
+                  cb(err);
+                } else {
+                  var resultLastVersion = bill['last_version_on'];
 
-              //invoke the callback only if all keys processed and the last bill in the final key has been processed
-              if (tasksTotal === Object.keys(allKeys).length && billsToGo === 0) {
-                if (result.length === 0) {
-                  result = '<h3>No new bills related to your topics today.</h3>';
+                  //format result date for comparison 
+                  resultDate = new Date(resultLastVersion);
+                  resultDate = resultDate.setDate(resultDate.getDate() + 1);
+
+                  console.log('result date', resultDate);
+                  console.log('last bill sent', lastBillsSent);
+
+                  if (resultDate > lastBillsSent) {
+                    console.log('*****************TRUEEEEEEEEEEEEEEE*****************');
+                    result += "<h2> Keyword: " + key + "</h2>";
+                    result += "<h4 style='color:grey;'> Bill ID: " + bill['bill_id'] + "</h2>";
+                    result += '<h3>' + bill['official_title'] + '</h3>';
+                    result += '<h3>--------------------------</h3>';
+                  }
+                  billsToGo--;
+                  allKeys[key] = key;
+
+                  //invoke the callback only if all keys processed and the last bill in the final key has been processed
+                  if (tasksTotal === Object.keys(allKeys).length && billsToGo === 0) {
+                    if (result.length === 0) {
+                      result = '<h3>No new bills related to your topics today.</h3>';
+                    }
+
+                    //write to logFile here, need to grab all bills and then report the most current 'last_version_on.'
+
+                    var lastVersion;
+                    var rawDate;
+
+                    //convert the format of 'last_version', then compare. searches to find most recent
+                    var countVersion = 0;
+                    Bill.find({}, function(err, resultBills) {
+                      if (err) {
+                        console.log('Could not find Bill database', err);
+                      } else {
+                        var amountResult = resultBills.length;
+                        resultBills.forEach(function(checkBill) {
+                          countVersion++;
+                          var dateBill = new Date(checkBill['last_version_on']);
+                          dateBill = dateBill.setDate(dateBill.getDate() + 1);
+                          if (lastVersion === undefined || lastVersion < dateBill) {
+                            lastVersion = dateBill;
+                            rawDate = checkBill['last_version_on'];
+                          }
+                          //console.log('this is the rawDate', rawDate);
+                          //console.log('this is the countVersion', countVersion);
+                          //console.log('amount result', amountResult);
+                          if (countVersion === amountResult) {  
+                            console.log('got in here');
+                            createEmailTimestamp(rawDate, function() {
+                              cb(null, result);
+                              //console.log(result);
+                              //process.exit();
+                            });
+                          }
+                        });
+                      }
+                    });
+                  }
                 }
-                cb(null, result);
-              }
+              });
             }
-          });
+          })(key);
         }
-      })(key);
+      }
     }
-  }
+  });
 };
 
 exports.sendMail = function(userObj, cb) {
@@ -93,13 +178,13 @@ exports.sendMail = function(userObj, cb) {
   //start construction body of email
   let insertHtml = "<h1>Here's what's happening today in congress. Visit <a href='http://159.203.239.137:8080/' style='color:blue;'>Legislation Watch</a> for more results!<br>";
 
-  //FOR TESTING ONLY: 2017-01-09
-  // var date = '2017-01-05';
+  //FOR THE DEMO ONLY: 2017-02-03
+  // var date = '2017-02-03';
 
-  //USE THIS DATE FOR THE REAL THING - Grabs today's date
+  //USE THIS DATE FOR THE REAL THING - Grabs today's date to include in subject line
   var date = formatDate();
 
-  addBills(userObj.keywords, date, function(err, result) {
+  addBills(userObj.keywords, function(err, result) {
     if (err) {
       console.log('Something went wrong');
       return;
